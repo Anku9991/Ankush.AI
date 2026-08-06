2f1bc34eee7d7345470a16bbabd6f78646e7567a
Ankush <jhaankush331@gmail.com>
2026-08-06 17:43:55 +0530

    perf: hide sticky CTA on mobile and defer FontAwesome load for instant First Contentful Paint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -390,8 +390,11 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
-
+        <link rel="preload" as="style" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" media="print" onLoad={(e) => { (e.target as HTMLLinkElement).media = 'all'; }} />
+        <noscript>
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+        </noscript>
         {/* Theme color for mobile browser chrome */}
         <meta name="theme-color" content="#0B1B3E" />
         <meta name="msapplication-TileColor" content="#0B1B3E" />
